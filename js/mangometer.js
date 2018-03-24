1b45559a24301ce219cc3bc8425987f3e3d9674d
Ruby Dong <ruby.dong@stonybrook.edu>
2018-03-23 20:27:20 -0400

    header file, signin, register, review forms done

--- js/mangometer.js
+++ js/mangometer.js
@@ -1,12 +1,9 @@
 function generateMangoes() {
 	var ratings = document.getElementsByClassName("rating");
-	console.log(ratings);
-
 		
 	for(var i = 0; i < ratings.length; i++) {	
 		var mangoes = Math.round((ratings[i].getAttribute("name"))/20);
 		var ratingDiv = ratings[i];
-		console.log("index " + i);
 		for (var j = 0; j < 5 - mangoes; j++) {
 			ratingDiv.innerHTML = "<img src='images/mangobw.png'>" + ratingDiv.innerHTML;
 		}
@@ -14,7 +11,5 @@ function generateMangoes() {
 		for (var j = 0; j < mangoes; j++) {
 			ratingDiv.innerHTML = "<img src='images/mangocolor.png'>" + ratingDiv.innerHTML;
 		}
-		console.log("exited");
 	}
-	console.log("aaa");
 }
