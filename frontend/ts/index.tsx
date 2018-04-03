3def29fe992d418c420083836331ecf4d054bb6d
Ruby Dong <ruby.dong@stonybrook.edu>
2018-04-03 04:29:22 -0400

    made header links the style again, moved around imports

--- frontend/ts/index.tsx
+++ frontend/ts/index.tsx
@@ -1,13 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import "../css/bootstrap.css";
-import "../css/style.css";
 import { BrowserRouter } from 'react-router-dom'
 import  App  from './components/App';
-import { Header } from "./components/header";
-import { Mangoes } from "./components/mangoes";
-
-const data = require('../json/index.json');
 
 ReactDOM.render((
   <BrowserRouter>
