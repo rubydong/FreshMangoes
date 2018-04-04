e062fb53ea1db8932e0babf49c82e6e0ad15b5e0
jackzheng2496 <jackzheng2496@gmail.com>
2018-04-03 21:24:00 -0400

    rating module changes

--- src/main/java/com/freshmangoes/app/rating/RatingController.java
+++ src/main/java/com/freshmangoes/app/rating/RatingController.java
@@ -1,21 +1,36 @@
 package com.freshmangoes.app.rating;
 
-
+import com.freshmangoes.app.rating.data.UserType;
 import com.freshmangoes.app.rating.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class RatingController {
   @Autowired
   private RatingService ratingService;
 
-  @RequestMapping(value = "/rating/{contentId}", method = RequestMethod.POST)
-  public boolean addRating(@PathVariable int contentId) {
-    return ratingService.addToRating(null);
-  }
+  //@Autowired
+  //private HttpSession httpSession;
 
+  @RequestMapping(value = "/rating/{contentId}", method = RequestMethod.POST)
+  public boolean addRating(@PathVariable Integer contentId,
+                           @RequestParam Integer score,
+                           @RequestParam String body) {
+    // Replace 1337 with session
+    return ratingService.addToRating(contentId,
+                                     score,
+                                     UserType.Audience,
+                           1337,
+                                     body);
+  }
 }
