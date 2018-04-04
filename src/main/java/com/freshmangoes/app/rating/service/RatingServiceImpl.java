fd8e858319fe4fed7fb957ee97e464adf71f5e59
jackzheng2496 <jackzheng2496@gmail.com>
2018-04-03 21:24:00 -0400

    rating module changes

--- src/main/java/com/freshmangoes/app/rating/service/RatingServiceImpl.java
+++ src/main/java/com/freshmangoes/app/rating/service/RatingServiceImpl.java
@@ -1,6 +1,7 @@
 package com.freshmangoes.app.rating.service;
 
 import com.freshmangoes.app.rating.data.Rating;
+import com.freshmangoes.app.rating.data.UserType;
 import com.freshmangoes.app.rating.repository.RatingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,18 @@ public class RatingServiceImpl implements RatingService {
   @Autowired
   private RatingRepository ratingRepository;
 
-  public boolean addToRating(Rating rating) {
-    return ratingRepository.insertRating(rating);
+  public boolean addToRating(Integer contentId,
+                             Integer score,
+                             UserType type,
+                             Integer reviewerId,
+                             String body) {
+    return ratingRepository.insertRating(Rating
+                                          .builder()
+                                          .contentId(contentId)
+                                          .score(score)
+                                          .type(type)
+                                          .reviewerId(reviewerId)
+                                          .body(body)
+                                          .build());
   }
 }
