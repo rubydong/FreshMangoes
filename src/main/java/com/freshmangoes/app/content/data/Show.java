e445cf6185116903249ef35eeab3da34356ada5f
jackzheng2496 <jackzheng2496@gmail.com>
2018-04-03 21:43:14 -0400

    added ratings and ratings

--- src/main/java/com/freshmangoes/app/content/data/Show.java
+++ src/main/java/com/freshmangoes/app/content/data/Show.java
@@ -1,7 +1,10 @@
 package com.freshmangoes.app.content.data;
 
 import com.freshmangoes.app.common.data.Media;
+import com.freshmangoes.app.rating.data.Rating;
 import lombok.Builder;
+
+import java.net.URL;
 import java.util.List;
 
 
@@ -14,11 +17,15 @@ public class Show extends Content {
               ContentType type,
               Media media,
               ContentMetadata contentMetadata,
-              List<Season> seasons) {
+              List<Season> seasons,
+              List<Rating> ratings,
+              URL summaryPhoto) {
     super.setId(id);
     super.setMedia(media);
     super.setMetadata(contentMetadata);
     super.setType(type);
+    super.setSummaryPhoto(summaryPhoto);
+    super.setRatings(ratings);
     this.seasons = seasons;
   }
 }
