e2ed7a50e7fadd7133ed3571a314dea9271810db
jackzheng2496 <jackzheng2496@gmail.com>
2018-04-04 20:11:02 -0400

    deleted unnecessary imports

--- src/main/java/com/freshmangoes/app/content/repository/MySQLContentRepository.java
+++ src/main/java/com/freshmangoes/app/content/repository/MySQLContentRepository.java
@@ -8,18 +8,13 @@ import com.freshmangoes.app.rating.data.Rating;
 import com.freshmangoes.app.rating.data.UserType;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
-import com.google.common.collect.ImmutableMap;
-
-import org.springframework.stereotype.Repository;
 
 
 @Repository
