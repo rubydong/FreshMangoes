a82a52a9bb606c451bf2afa4c14ec5fab64c3999
jackzheng2496 <jackzheng2496@gmail.com>
2018-04-05 21:59:46 -0400

    really bad pair class

---
+++ src/main/java/com/freshmangoes/app/common/data/Pair.java
@@ -0,0 +1,23 @@
+package com.freshmangoes.app.common.data;
+
+import java.util.AbstractMap;
+
+
+
+public class Pair<F, S> extends AbstractMap.SimpleImmutableEntry<F, S> {
+  public Pair(F f, S s) {
+    super(f, s);
+  }
+
+  public F getFirst() {
+    return getKey();
+  }
+
+  public S getSecond() {
+    return getValue();
+  }
+
+  public String toString() {
+    return "[" + getKey() + "," + getValue() + "]";
+  }
+}
