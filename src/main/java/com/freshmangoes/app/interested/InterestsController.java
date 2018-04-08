ba5c1cb54d5b43488ea10f66078dd14e7240b0d1
Romin Persaud <rominpersaud@msn.com>
2018-04-08 17:37:28 -0400

    Implemented build script and removed excess annotations from source code.

--- src/main/java/com/freshmangoes/app/interested/InterestsController.java
+++ src/main/java/com/freshmangoes/app/interested/InterestsController.java
@@ -9,9 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-/**
- * Created by romin on 4/8/18.
- */
 public class InterestsController {
 
   @Autowired
--- src/main/java/com/freshmangoes/app/user/UserController.java
+++ src/main/java/com/freshmangoes/app/user/UserController.java
@@ -5,11 +5,8 @@ import com.freshmangoes.app.user.data.User;
 import com.freshmangoes.app.user.service.UserService;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
