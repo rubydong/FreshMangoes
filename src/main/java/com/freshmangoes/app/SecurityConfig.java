0c81ee1e73ab614275c3cb845ee78ec21a697f72
Romin Persaud <rominpersaud@msn.com>
2018-04-08 14:46:10 -0400

    Moved security config to root of app dir.

--- src/main/java/com/freshmangoes/app/SecurityConfig.java
+++ src/main/java/com/freshmangoes/app/SecurityConfig.java
@@ -1,3 +1,5 @@
+package com.freshmangoes.app;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
