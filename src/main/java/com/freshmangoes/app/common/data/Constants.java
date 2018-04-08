ca5174b66421a5bf774f2700c2cf66304bbbacc0
jackzheng2496 <jackzheng2496@gmail.com>
2018-04-08 18:18:28 -0400

    add/get ratings

--- src/main/java/com/freshmangoes/app/common/data/Constants.java
+++ src/main/java/com/freshmangoes/app/common/data/Constants.java
@@ -12,7 +12,8 @@ public class Constants {
   public static final String SHOW_MAPPING = "/show/{id}";
   public static final String SEASON_MAPPING = "/show/*/{seasonId}";
   public static final String EPISODE_MAPPING = "/show/*/*/{episodeId}";
-  public static final String RATING_MAPPING = "/rating/{contentId}";
+  public static final String ADD_RATING_MAPPING = "/rating/{contentId}";
+  public static final String GET_RATING_MAPPING = "/rating/search";
 
   public static final String PROFILE_MAPPING = "/profile/{userId}";
 
@@ -20,4 +21,6 @@ public class Constants {
   public static final String DISPLAY_NAME = "displayName";
   public static final String EMAIL = "email";
   public static final String PASSWORD = "password";
+  public static final String BODY = "body";
+  public static final String SCORE = "score";
 }
--- src/main/java/com/freshmangoes/app/rating/RatingController.java
+++ src/main/java/com/freshmangoes/app/rating/RatingController.java
@@ -1,15 +1,18 @@
 package com.freshmangoes.app.rating;
 
 import com.freshmangoes.app.common.data.Constants;
+import com.freshmangoes.app.rating.data.Rating;
 import com.freshmangoes.app.rating.service.RatingService;
 import com.freshmangoes.app.user.data.userType;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class RatingController {
   @Autowired
   private RatingService ratingService;
@@ -17,16 +20,29 @@ public class RatingController {
   @Autowired
   private HttpSession session;
 
-  @PostMapping(Constants.RATING_MAPPING)
-  public ResponseEntity addRating(@PathVariable final Integer contentId,
-                                  @RequestParam final Integer score,
-                                  @RequestParam final String body) {
+  @PostMapping(Constants.ADD_RATING_MAPPING)
+  public ResponseEntity addRating(@RequestBody final Map<String, String> body,
+                                  @PathVariable Integer contentId) {
     boolean result = ratingService.addToRating(contentId,
-                                               score,
+                                               Integer.parseInt(body.get(Constants.SCORE)),
                                                userType.AUDIENCE,
                                                (Integer) session.getAttribute(Constants.USER_ID),
-                                               body);
+                                               body.get(Constants.BODY));
     return result ? ResponseEntity.ok("Rating added successfully")
                   : ResponseEntity.status(490).body("Something went wrong");
+  }
+
+  @GetMapping("/rating/search")
+  public List<Rating> getRating(@RequestParam("contentId") Optional<String> contentId,
+                                @RequestParam("reviewerId") Optional<String> reviewerId) {
+    if (!contentId.isPresent() && !reviewerId.isPresent()) {
+      return ratingService.getRatingByReviewerId((Integer)session.getAttribute(Constants.USER_ID));
+    } else if (reviewerId.isPresent()) {
+      System.out.println(reviewerId);
+      return ratingService.getRatingByReviewerId(Integer.parseInt(reviewerId.get()));
+    } else {
+      System.out.println(contentId);
+      return ratingService.getRatingByContentId(Integer.parseInt(contentId.get()));
+    }
   }
 }
