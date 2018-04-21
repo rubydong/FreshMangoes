7fa1c3d2afa2fabb45bc1b2a4a56e36eb2f8cd8d
Ruby Dong <ruby.dong@stonybrook.edu>
2018-04-21 16:42:49 -0400

    maybe some logic set up for search switch?

--- frontend/ts/templates/SearchTemplate.tsx
+++ frontend/ts/templates/SearchTemplate.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Search }  from "../types/content";
 import { SearchCriteriaComponent } from "../components/SearchCriteriaComponent";
 import { ContentListsComponent } from "../components/ContentListsComponent";
+import { SpotlightDetailComponent } from "../components/SpotlightDetailComponents";
 
 export class SearchTemplate extends React.Component {
     state : Search;
@@ -22,6 +23,12 @@ export class SearchTemplate extends React.Component {
         }
     }
 
+    setSelectedContent(content, title) {
+        this.state.selectedContent = content;
+        this.state.selectedTitle = title;
+        this.forceUpdate()
+    }
+
     render() {
         return (
         <div className="search">
@@ -33,15 +40,20 @@ export class SearchTemplate extends React.Component {
                 <div className="search-results">
                     <h4> {this.state.results} Results found for "{window.location.search.substring(7).replace("%20", " ")}"</h4> <hr/>
                     <ul className="list-inline">
-                        <li className="underline"><a href="">All</a></li>
-                        <li><a href="">Movies</a></li>		
-                        <li><a href="">TV Shows</a></li>		
-                        <li><a href="">Celebrities</a></li>
+                        <li><button className="btn-link" onClick={() => this.setSelectedContent([], 'All')}>All</button></li>
+                        <li><button className="btn-link" onClick={() => this.setSelectedContent(this.state.movies, 'Movies')}>Movies</button></li>		
+                        <li><button className="btn-link" onClick={() => this.setSelectedContent(this.state.shows, 'TV Shows')}>TV Shows</button></li>		
+                        <li><button className="btn-link" onClick={() => this.setSelectedContent(this.state.celebrities, 'Celebrities')}>Celebrities</button></li>
                     </ul>
                     
+                    {this.state.selectedContent == [] || this.state.selectedContent == undefined
+                    ? <div>
                     <ContentListsComponent data-title='Movies' data-content={this.state.movies} data-search="true"/>
                     <ContentListsComponent data-title='TV Shows' data-content={this.state.shows} data-search="true"/>
                     <ContentListsComponent data-title='Celebrities' data-content={this.state.celebrities} data-search="true"/>
+                    </div>
+                    : <SpotlightDetailComponent data-content={this.state.selectedContent} data-title={this.state.selectedTitle}/>
+                    }
                 </div>			
             </div>
         </div>
