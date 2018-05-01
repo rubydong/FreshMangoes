e20be745ee07ed3793191078da0429936aea759b
Ruby Dong <ruby.dong@stonybrook.edu>
2018-04-30 22:56:00 -0400

    fixed critic template + delete button

--- frontend/ts/components/DetailsComponent.tsx
+++ frontend/ts/components/DetailsComponent.tsx
@@ -69,10 +69,8 @@ export class DetailsComponent extends React.Component {
                 {currentUser && currentUser.userType == UserType.ADMIN 
                 ? <span className="rating">
                     <b>Edit Information:</b>  <img src={EDIT_ICON} data-toggle="modal" data-target="#edit-info-modal"/> <br/>
-                    <b>Delete Page:</b> <button onClick={() => {
-                        if (window.confirm("Are you sure you want to delete this content?"))
-                            this.deleteContent() }}>
-                        <img src={TRASH_ICON}/></button><br/>
+                    <b>Delete Page:</b>
+                        <img src={TRASH_ICON} onClick={() => { if (window.confirm("Are you sure you want to delete this content?")) this.deleteContent() }}/><br/>
                   </span>
                 : ''}
 
