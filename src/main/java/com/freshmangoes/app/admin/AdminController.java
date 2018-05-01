0fcd041692ed0624e9932454a37065896149bbbb
jackzheng2496 <jackzheng2496@gmail.com>
2018-04-30 22:00:02 -0400

    adding content/deleting content done

--- src/main/java/com/freshmangoes/app/admin/AdminController.java
+++ src/main/java/com/freshmangoes/app/admin/AdminController.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,14 +68,14 @@ public class AdminController {
     return null;
   }
 
-  @PostMapping(Constants.ADMIN_DELETE_DETAIL_PAGE_MAPPING)
-  public ResponseEntity deleteDetailPage(@RequestBody final Map<String, String> body,
+  @DeleteMapping(Constants.ADMIN_DELETE_DETAIL_PAGE_MAPPING)
+  public ResponseEntity deleteDetailPage(@PathVariable final String contentType,
                                          @PathVariable final Integer contentId) {
     final HttpStatus status;
 
     if (adminService.isAuthenticatedAdmin(session)) {
       status = HttpStatus.OK;
-      adminService.deleteDetailPage(contentId, ContentType.valueOf(body.get("type")));
+      adminService.deleteDetailPage(contentId, ContentType.valueOf(contentType));
     } else {
       status = HttpStatus.BAD_REQUEST;
     }
@@ -90,7 +91,7 @@ public class AdminController {
     return reports;
   }
 
-  @PostMapping(Constants.ADMIN_DELETE_RATING_MAPPING)
+  @DeleteMapping(Constants.ADMIN_DELETE_RATING_MAPPING)
   public ResponseEntity deleteRating(@PathVariable final Integer ratingId) {
     final HttpStatus status;
 
@@ -104,7 +105,7 @@ public class AdminController {
     return ResponseEntity.status(status).build();
   }
 
-  @PostMapping(Constants.ADMIN_DELETE_USER_MAPPING)
+  @DeleteMapping(Constants.ADMIN_DELETE_USER_MAPPING)
   public ResponseEntity deleteUser(@PathVariable final Integer userId) {
     final HttpStatus status;
 
