9b5351a67b3124a8f0336c0d8f29249bfdd4c931
Ubuntu <rlpersaud@webapp.jxcsnr5tsnxevb4sbn3vif0tvh.bx.internal.cloudapp.net>
2018-05-02 08:03:09 +0000

    image paths for createcontent and celebrities modified

--- frontend/ts/components/CastComponent.tsx
+++ frontend/ts/components/CastComponent.tsx
@@ -3,11 +3,18 @@ import { NO_USER_PHOTO, CAST_LIMIT, EDIT_ICON } from "../../GlobalVariables";
 import { UserType } from '../types/user';
 
 export class CastComponent extends React.Component {
+
+    getCelebrityImage(path) {
+        if (path.startsWith('http')) {
+	    return path;
+        }
+        return window.location.origin + '/media/profiles/' + path;
+    }
     render() {
         const currentUser = this.props['data-current-user'];
         const cast = this.props['data-cast'].slice(0, CAST_LIMIT).map((castPerson, i) => {
             return <div className="cast-person" key={i}>
-                <img className="img-align-left" src={castPerson.celebrity.profilePicture ? castPerson.celebrity.profilePicture.path : NO_USER_PHOTO}/>
+                <img className="img-align-left" src={castPerson.celebrity.profilePicture ? this.getCelebrityImage(castPerson.celebrity.profilePicture.path) : NO_USER_PHOTO}/>
                 <b> <a href={"/../celebrity/" + castPerson.celebrity.id}>{castPerson.celebrity.name}</a> </b> <br/>
                 <i>{castPerson.role}</i>
             </div>
