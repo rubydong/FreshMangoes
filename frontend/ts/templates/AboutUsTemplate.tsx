fa0b5d75dfa9f06947cc929a35a29548c336cccf
normanle <norman.le@stonybrook.edu>
2018-05-02 01:47:08 -0400

    about us

---
+++ frontend/ts/templates/AboutUsTemplate.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+export class AboutUsTemplate extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <div className="white-component margin-top-bottom">
+                    <h2> About Fresh Mangoes </h2> <p/>
+
+                    Fresh Mangoes® and the Mangometer® score are the freshest recommendation sources for entertainment worldwide. As one of the largest online aggregator of movie and TV show reviews from professional critics, Fresh Mangoes offers the most comprehensive guide to what's Fresh. The Mangometer score represents the percentage of positive professional reviews for films and TV shows and is used by fans to help with their viewing decisions.
+                    In 2018, Fresh Mangoes was created to serve moviegoers and entertainment fans. The idea was made to bring new features to existing web services. Made for CSE 308 Spring 2018 taught by Professor Kelly.
+                    <p/>
+                    WHAT IS THE CERTIFIED FRESH?
+                    <p/>
+                    We use our trusted Mango score to find the 30 best shows judged by our handpicked critics.
+                    You can apply to be a critic and we’ll determine if you fit our criteria!
+                    <p/>
+                    WHAT IS THE MANGOMETER®?
+                    <p/>
+                    The Mangometer score – based on the opinions of tens of critics – is a trusted measurement of critical recommendation for all of our fans.
+                    The Mangometer score represents the percentage of professional critic reviews that are positive for a given film or television show.
+                    <br/>
+                    WHAT IS THE AUDIENCE SCORE?
+                    <p/>
+                    The Audience rating is the percentage of all users who have rated the movie or TV Show.
+
+                    The Interested Button will add TV shows and movies to your own personal list. If you are not interested in one, you can add that to a list as well with the uninterested button.
+
+                </div>
+            </div>
+        );
+    }
+}
